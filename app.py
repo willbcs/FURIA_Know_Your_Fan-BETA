99e2c1f1b4da7d98e995e933f7a156a228b02fa2
William Bruno <willbc.silva@gmail.com>
2025-05-04 13:46:20 -0300

    aupdate

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ def index():
         
     return render_template('index.html')
 
-app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'GET':
         return render_template('upload.html')
